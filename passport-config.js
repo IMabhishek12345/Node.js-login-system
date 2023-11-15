429f0656f0b711dd8e2d17f3106b106e4f7a95a9
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2023-11-16 01:28:07 +0530

    authenticate the original user to the home page

--- passport-config.js
+++ passport-config.js
@@ -1,15 +1,41 @@
-const { authenticate } = require("passport");
-const localStratergy=require("passport-local");
+const { authenticate }=require("passport");
+const localStrategy=require("passport-local").Strategy;
+const bcrypt=require("bcrypt");
 
-function initialize(passport){
-    const authenticateUser=(email,password,done)=>{
-      const user=getUserByEmail(email)
-      if(user== null){
-        return done(null,false,{message:"No user witn that email exists"});
+function initialize(passport,getUserByEmail,getUserbyId){
+      const authenticateUser=async(email,password,done)=>{
+      const user=getUserByEmail(email);
+      if(user=== null){
+      //In done(err,userexist,object):- err is the error in the server 
+      return done(null,false,{message:"No user witn that email exists"});
+      }
+      try {
+        if (await bcrypt.compare(password,user.password)){
+          return done(null,user)
+        }else{
+          return done(null,false,{message: "Password Incorrect"})
+        }
+      } 
+      catch (err) {
+        return done(err);//return the error in our application
       }
     }
-    passport.use(new localStratergy({usernameField: "email"}),authenticateUser)
+    passport.use(new localStrategy({usernameField: "email"},authenticateUser));
 
-    passport.serializeUser((user,done)=>{});
-    passport.deserializeUser((id,done)=>{});
+    passport.serializeUser((user,done)=>done(null,user.id));
+    passport.deserializeUser((id,done)=>{ return done(null,getUserbyId(id))});
 }
+//serializeUser determines which data of the user object should be stored 
+//in the session. The result of the serializeUser method is attached to the
+//session as req.session.passport.user = {}. Here for instance, 
+//it would be (as we provide the user id as the key) req.session.passport.user 
+//= {id: 'xyz'}
+
+//The first argument of deserializeUser corresponds to the key of the user
+//object that was given to the done function (see 1.). So your whole object
+//is retrieved with help of that key. That key here is the user id (key can 
+//be any key of the user object i.e. name,email etc). 
+//In deserializeUser that key is matched with the in memory 
+//array / database or any data resource.
+
+module.exports=initialize;
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+if (process.env.NODE_ENV!=="production"){
+    require("dotenv").config()
+}
+
 const express=require("express");
 const app=express();
 const bcrypt=require("bcrypt");
 const passport=require("passport");
+const flash=require("express-flash");
+const session=require("express-session");
+
 const initializePassport=require("./passport-config");
-initializePassport(passport);
 
 const users=[];//for holding different users data, if we are not 
+
+initializePassport(passport,email =>
+    users.find(user=> user.email===email),
+    id => users.find(user=> user.id===id)
+);
+
 //connected to any database
 
 app.set("view-engine","ejs")
@@ -23,6 +35,23 @@ app.use(express.urlencoded({extended: false}));//extended:false means
 //it parse only incoming request object like arrays or strings
 //extended:true means it parse any incoming request object with nested objects etc.
 
+app.use(flash());
+//HTTP is stateless; in order to associate a request to any other request,
+//you need a way to store user data between HTTP requests. Cookies and URL
+//parameters are both suitable ways to transport data between the client 
+//and the server. But they are both readable and on the client side. 
+//Sessions solve exactly this problem. You assign the client an ID and it 
+//makes all further requests using that ID. Information associated with the 
+//client is stored on the server linked to this ID.
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false  //This is for saving a empty value if there is
+                              // no value that's why false is stored 
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/",(req,res)=>{
     res.render("index.ejs",{name: "abhishek"})
@@ -34,9 +63,12 @@ app.get("/login",(req,res)=>{
     res.render("login.ejs");
 })
 
-app.post("/login",(req,res)=>{
-    
-})
+app.post("/login",passport.authenticate("local",{
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureflash: true
+  
+}))
 app.post("/register",async(req,res)=>{
   try {
     const hashedPassword=await bcrypt.hash(req.body.password,10);
